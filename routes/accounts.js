25b193000a5c5d92b7c6feaf9fbbed55d4f59c25
RotaruDan <drotaru@ucm.es>
2015-06-29 13:42:28 +0200

    Adds models structure

---
+++ routes/accounts.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express'),
+	router = express.Router();
+
+/* GET users listing. */
+router.get('/signup', function(req, res, next) {
+  
+});
+
+module.exports = router;
