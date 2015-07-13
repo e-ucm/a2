827e0671317f0168032a9c0a30a36fa5c8c8ab6e
gorco <antcal01@ucm.es>
2015-07-14 00:30:32 +0200

    Moved /reset, /forgot and /contact GETs. Also formatted the file.

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,43 @@
 var express = require('express'),
-	router = express.Router();
+    router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gleaner Users' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Gleaner Users'
+    });
+});
+
+/* GET contact page. */
+router.get('/contact', function (req, res, next) {
+    res.render('contact', {});
 });
 
 /* GET login page. */
-router.get('/login', function(req, res, next) {
-    res.render('login', {
+router.get('/login', function (req, res, next) {
+    res.render('login', {});
+});
+
+/* GET forgot page. */
+router.get('/login/forgot', function (req, res, next) {
+    res.render('forgot', {});
+});
+
+/* GET reset page. */
+router.get('/login/reset/:token', function (req, res, next) {
+    res.render('reset', {
+        token: req.params.token
     });
 });
 
 /* GET signup page. */
-router.get('/signup', function(req, res, next) {
-    res.render('signup', {
-    });
+router.get('/signup', function (req, res, next) {
+    res.render('signup', {});
 });
 
 /* GET users list view. */
 router.get('/users', function (req, res, next) {
-    res.render('users', {
-    });
+    res.render('users', {});
 });
 
 /* GET pofile view. */
@@ -30,6 +46,5 @@ router.get('/users/:userId', function (req, res, next) {
         userId: req.params.userId
     });
 });
-
 
 module.exports = router;
