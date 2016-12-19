9c99fc5e66f47502b580c83342dc74dc8c1d55e9
RotaruDan <drotaru@ucm.es>
2016-12-19 15:01:25 +0100

    Adds loginPlugins mongo collection

--- models.js
+++ models.js
@@ -20,4 +20,5 @@
 exports = module.exports = function (app, mongoose) {
     require('./schema/user')(app, mongoose);
     require('./schema/application')(app, mongoose);
+    require('./schema/loginPlugin')(app, mongoose);
 };
